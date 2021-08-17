b4fb7dd485f5479b56c0401443cad0e07c90244f
firuza <firya87@gmail.com>
2021-08-17 20:47:39 +0530

    feat: update release config

--- release.config.js
+++ release.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   "plugins": [
-    "@semantic-release/commit-analyzer",
+    ["@semantic-release/commit-analyzer", {
+      "preset": "angular",
+      "releaseRules": [
+        {"type": "docs", "scope":"README", "release": "patch"},
+        {"type": "refactor", "release": "patch"},
+        {"type": "module", "release": "feat"},
+        {"type": "style", "release": "patch"}
+      ],
+      "parserOpts": {
+        "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES"]
+      }
+    }],
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     ["@semantic-release/npm", {
@@ -11,5 +22,4 @@ module.exports = {
     }],
     "@semantic-release/git"
   ],
-  "preset": "angular"
 }
